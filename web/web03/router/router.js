12bb1790d7242d28995a8888ddb50b751b835b1c
bunnanna <na.nuttanont@gmail.com>
2022-12-09 15:54:21 +0900

    fix header

--- web/web03/router/router.js
+++ web/web03/router/router.js
@@ -1,29 +1,45 @@
+const { promiseImpl } = require("ejs")
 const express = require("express")
 const router = express.Router()
 const path = require("path")
 const mongo = require('../model/mongo')
-let chk=null
+let chk = null
+const cookie_time = 10*60*1000
 
 router.get("/", (req, res) => {
-    res.render("../views/login.ejs",{chk})
+    if (req.cookies.login) {
+        res.render("../views/home.ejs", { role: req.cookies.userrole,name:req.cookies.username })
+    } else {
+        res.render("../views/login.ejs", { role: null, chk: chk })
+    }
+
 })
 
 router.post("/login", (req, res) => {
     const idpw = req.body
     mongo.findOne({ username: idpw.UserId }).then((doc) => {
-            if (idpw.password === doc.password) {
-                res.cookie("userrole",doc.role,{maxAge:10*60*1000})
-                res.cookie("login",true)
-                chk=null
-                res.render("../views/home.ejs")
-            } else {
-                chk="Wrong password Try again"
-                res.redirect("/")
-            }
+        if (idpw.password === doc.password) {
+                res.cookie("userrole", doc.role, { maxAge:cookie_time})
+                res.cookie("login", true, { maxAge:cookie_time})
+                res.cookie("username", doc.username, { maxAge:cookie_time})
+                chk = null
+        } else {
+            chk = "Wrong password Try again"
         }
-    ).catch((err)=>{
-        chk="Does't found This ID"
-        res.redirect("/")})
+        res.redirect("/")
+    }
+    ).catch((err) => {
+        chk = "Does't found This ID"
+        res.redirect("/")
+    })
+})
+
+router.get("/logout", (req, res) => {
+    res.clearCookie("userrole")
+    res.clearCookie("login")
+    res.clearCookie("username")
+    chk = null
+    res.redirect("/")
 })
 
 // const basepath =path.join(__dirname,"..\\webpage")
